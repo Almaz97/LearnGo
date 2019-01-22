87813bc92006e93ad822a61d9f2099981325a2ce
Almaz <almaz.carato@gmail.com>
2019-01-22 21:36:06 +0600

    Exploring golang fmt.Prinf function verbs

--- first/printf/main.go
+++ first/printf/main.go
@@ -15,4 +15,28 @@ func main() {
 		"total: %d, \nsucces: %d / %d\n",
 		ops, ok, fail,
 	)
+
+	// Escape sequences
+	fmt.Println("hi\\n\"hi\"")
+	fmt.Println("hi\"This message should appear in double quotes\"")
+
+	// Printing types
+	var off bool
+	fmt.Printf("%T\n", brand)
+	fmt.Printf("%T\n", ops)
+	fmt.Printf("%T\n", off)
+
+	// %v in printf
+	var (
+		planet   = "Venus"
+		distance = 261
+		orbital  = 224.701
+		hasLife  = false
+	)
+
+	fmt.Printf("Planet: %v\n", planet)
+	fmt.Printf("Distance: %v millions kms\n", distance)
+	fmt.Printf("Orbital Period: %v days\n", orbital)
+	fmt.Printf("Does %v have life? %v\n", planet, hasLife)
+	fmt.Printf("%v is %v away. Think! %[2]v kms! %[1]v OMG!\n", planet, distance)
 }
