cf952283b104a5b1bd34f56cd9f4f71e04cb9dc4
Almaz <almaz.carato@gmail.com>
2019-02-07 14:38:05 +0600

    Add test func, that asserts slice elements

--- deep_into_golang/cardsProject/deck_test.go
+++ deep_into_golang/cardsProject/deck_test.go
@@ -1,6 +1,9 @@
 package main
 
-import "testing"
+import (
+	"os"
+	"testing"
+)
 
 func TestNewDeck(t *testing.T) {
 	d := newDeck()
@@ -8,4 +11,18 @@ func TestNewDeck(t *testing.T) {
 	if len(d) != 16 {
 		t.Errorf("Expecting deck length of 16, but got, %d", len(d))
 	}
+}
+
+func TestSaveToDeckAndNewDeckFromFile(t *testing.T) {
+	os.Remove("_deckTesting")
+
+	d := newDeck()
+	d.saveToFile("_deckTesting")
+
+	loadedDeck := newDeckFromFile("_deckTesting")
+	if len(loadedDeck) != 16 {
+		t.Errorf("Expecting deck length of 16, but got, %d", len(d))
+	}
+
+	os.Remove("_deckTesting")
 }
