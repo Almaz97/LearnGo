671a3a392b201bf01d64c5e954f7de12fc96d3c0
Almaz <almaz.carato@gmail.com>
2019-02-07 09:58:28 +0600

    Add newDeck() func, to create a deck of cards

--- deep_into_golang/cardsProject/main.go
+++ deep_into_golang/cardsProject/main.go
@@ -1,17 +1,10 @@
 package main
 
-import "fmt"
-
 func main() {
-	cards := deck{"Ace of Diamonds", newCard()}
-	cards = append(cards, "Six of Spades")
+	cards := newDeck()
 
-	for i, card := range cards {
-		fmt.Println(i, card)
-	}
-	//fmt.Println(cards)
-}
+	hand, remainingCards := deal(cards, 5)
 
-func newCard() string {
-	return "Five of Diamonds"
+	hand.print()
+	remainingCards.print()
 }
